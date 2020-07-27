7a68576123049e20c780938a774f855cccc7ab13
Miss-Jaiswal <58948075+Miss-Jaiswal@users.noreply.github.com>
2020-07-27 14:00:47 +0530

    Add files via upload

---
+++ batch_25_Day_2.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#strings triple quotes
+c ='''Shraddha'''
+print(c)
+
+
+# In[4]:
+
+
+#strings double quotes
+c = "Shraddha"
+print(c)
+
+
+# In[5]:
+
+
+#strings single quotes
+c ='Shraddha'
+print(c)
+
+
+# In[6]:
+
+
+type(c)
+
+
+# In[7]:
+
+
+d='shraddha jaiswal'
+print(d)
+
+
+# In[8]:
+
+
+print(d.title())
+
+
+# In[9]:
+
+
+print(d.upper())
+
+
+# In[10]:
+
+
+print(d.lower())
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ batch_25_day_3.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+first_name="shraddha"
+last_name="jaiswal"
+full_name=f"{first_name} {last_name}"
+print(full_name)
+
+
+# In[19]:
+
+
+#Adding whitespaces to string
+print("without using any whitespaces")
+print("\nFav languages are:Pythonjavarubyccobalc++")
+print("\nUsing newline delimeter\n")
+print("Fav languages are:\nPython\njava\nruby\nc\ncobal\nc++")
+print("\nUsing newline and tab delimeter\n")
+print("Fav languages are:\n\t\tPython\n\tjava\n\truby\n\tc\n\tcobal\n\tc++")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
